1e0f1e12ce3af3554bb74834f203c18cec3e6bd1
liuguangxin <liuguangxin@jd.com>
2021-02-18 18:50:30 +0800

    添加注释

--- javartisan-app/src/main/java/com/javartisan/app/GoodsController.java
+++ javartisan-app/src/main/java/com/javartisan/app/GoodsController.java
@@ -1,6 +1,7 @@
 package com.javartisan.app;
 
 import com.javartisan.goods.api.GoodApi;
+import com.javartisan.goods.api.SensitiveGoodApi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,10 +15,20 @@ public class GoodsController {
     @Autowired
     private GoodApi goodApi;
 
+    @Autowired
+    private SensitiveGoodApi sensitiveGoodApi;
+
     @RequestMapping("get")
     public String get() {
         String value = goodApi.get("" + new Random().nextInt(100));
-        System.out.println(value);
+        System.out.println("goodApi = " + value);
+        return value;
+    }
+
+    @RequestMapping("get2")
+    public String get2() {
+        String value = sensitiveGoodApi.get("" + new Random().nextInt(100));
+        System.out.println("sensitiveGoodApi = " + value);
         return value;
     }
 }
--- javartisan-goods/src/main/java/com/javartisan/goods/api/SensitiveGoodApi.java
+++ javartisan-goods/src/main/java/com/javartisan/goods/api/SensitiveGoodApi.java
@@ -5,11 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 /**
- * name 指定对应的应用名称.
- * contextId：如果bean名字存在的使用contextId作为bean名字，解决Spring Bean Definition冲突问题
+ * name 指定对应的应用名称
  */
-@FeignClient(name = "good-service-app", contextId = "GoodApi")
-public interface GoodApi {
+@FeignClient(name = "good-service-app", contextId = "SensitiveGoodApi")
+public interface SensitiveGoodApi {
 
     @RequestMapping("/good/get")
     public String get(@RequestParam("name") String name);
