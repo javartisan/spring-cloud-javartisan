81d2c8bfa21e9453d1011caeb1bb235c6f09b490
liuguangxin <liuguangxin@jd.com>
2021-02-19 11:09:18 +0800

    fix  FeignClient scan error

--- javartisan-app/src/main/java/com/javartisan/app/JavartisanAppApplication.java
+++ javartisan-app/src/main/java/com/javartisan/app/JavartisanAppApplication.java
@@ -4,12 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
-@ComponentScan(basePackages = "com.javartisan")
+@EnableFeignClients(basePackages = {"com.javartisan"})
+/**
+ * ComponentScan 该注解是不会处理@FeignClient注解的扫描。需要使用EnableFeignClients指定@FeignClient扫描包路径
+ *
+ * @ComponentScan(basePackages = {"com.javartisan"})
+ */
 public class JavartisanAppApplication {
 
     public static void main(String[] args) {
