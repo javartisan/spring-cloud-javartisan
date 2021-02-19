af9b23dd2c840bcf67c8a95e2befd426bca5b16b
liuguangxin <liuguangxin@jd.com>
2021-02-19 11:33:48 +0800

    add Hystrix

--- javartisan-app/src/main/java/com/javartisan/app/GoodsController.java
+++ javartisan-app/src/main/java/com/javartisan/app/GoodsController.java
@@ -4,9 +4,8 @@ import com.javartisan.goods.api.GoodApi;
 import com.javartisan.goods.api.SensitiveGoodApi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Random;
 
 @RestController
 @RequestMapping(value = "api")
@@ -19,15 +18,15 @@ public class GoodsController {
     private SensitiveGoodApi sensitiveGoodApi;
 
     @RequestMapping("get")
-    public String get() {
-        String value = goodApi.get("" + new Random().nextInt(100));
+    public String get(@RequestParam(value = "name") String name) {
+        String value = goodApi.get(name);
         System.out.println("goodApi = " + value);
         return value;
     }
 
     @RequestMapping("get2")
-    public String get2() {
-        String value = sensitiveGoodApi.get("" + new Random().nextInt(100));
+    public String get2(@RequestParam(value = "name") String name) {
+        String value = sensitiveGoodApi.get(name);
         System.out.println("sensitiveGoodApi = " + value);
         return value;
     }
--- javartisan-goods/src/main/java/com/javartisan/goods/service/GoodServiceImpl.java
+++ javartisan-goods/src/main/java/com/javartisan/goods/service/GoodServiceImpl.java
@@ -1,12 +1,32 @@
 package com.javartisan.goods.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GoodServiceImpl implements GoodService {
 
     @Override
+    @HystrixCommand(fallbackMethod = "errorHandler")
     public String get(String name) {
-        return "参数name：" + name;
+        if ("error".equalsIgnoreCase(name)) {
+            int i = 1 / 0;
+        }
+        String value = "参数name：" + name;
+        return value;
+    }
+
+    // 熔断方法需要与原始对应方法参数保持一致
+    public String errorHandler(String name) {
+        return name + " errorHandler服务器繁忙：稍后再试!";
+    }
+
+    // 熔断方法需要与原始对应方法参数保持一致,否则报错：fallback method wasn't found: errorHandler([class java.lang.String])
+    public String errorHandler() {
+        return " errorHandler服务器繁忙：稍后再试!";
+    }
+
+    public String errorHandler(Object name) {
+        return name + " errorHandler服务器繁忙：稍后再试!";
     }
 }
