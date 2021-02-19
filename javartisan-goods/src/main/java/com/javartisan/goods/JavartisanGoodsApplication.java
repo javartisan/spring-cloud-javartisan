b136a43660524db9025ea19798af356236ba9466
liuguangxin <liuguangxin@jd.com>
2021-02-19 12:05:53 +0800

    add  hystrix

--- javartisan-goods/src/main/java/com/javartisan/goods/JavartisanGoodsApplication.java
+++ javartisan-goods/src/main/java/com/javartisan/goods/JavartisanGoodsApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableDiscoveryClient
 @EnableCircuitBreaker
+@EnableHystrixDashboard
 public class JavartisanGoodsApplication {
 
     public static void main(String[] args) {
